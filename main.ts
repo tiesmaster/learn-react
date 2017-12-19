f25ac4f0a885968f344e9f4798ffca0eb6a2cb0b
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-12-20 00:19:38 +0100

    Euler: problem 1 (1-9).

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-let n: never;
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let sum = 0;
+for (const i of list) {
+  console.log("i: " + i);
+  if (i % 3 === 0 || i % 5 === 0) {
+    console.log("i: mod 3/5: " + i);
+    sum += i;
+  }
+}
+
+console.log("sum of 1 - 9: " + sum);
