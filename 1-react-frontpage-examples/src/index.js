b804769849a8d213421fa6561b64b6b3952ba79e
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-12-27 01:04:23 +0100

    01: Create simple component.

--- 1-react-frontpage-examples/src/index.js
+++ 1-react-frontpage-examples/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+ReactDOM.render(<HelloMessage name="Ties" />, document.getElementById("root"));
