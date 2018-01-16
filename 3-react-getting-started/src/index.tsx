0a1bd42ff28687605dcd65203b8d6e224f194a71
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-01-16 23:39:46 +0100

    3: Extract a Clock element.

--- 3-react-getting-started/src/index.tsx
+++ 3-react-getting-started/src/index.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-function tick() {
-  const element = (
+function Clock(props: { date: Date }) {
+  return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
     </div>
   );
+}
+
+function tick() {
   ReactDOM.render(
-    element,
+    <Clock date={new Date()} />,
     document.getElementById('root')
   );
 }
