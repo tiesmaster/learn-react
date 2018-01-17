dac7775feede6753e6362894b269b0948c31ff55
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-01-17 18:46:46 +0100

    3: Add Toggle component.

--- 3-react-getting-started/src/index.tsx
+++ 3-react-getting-started/src/index.tsx
@@ -1,51 +1,27 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-interface ClockProps {
-  date: Date;
-}
-
-class Clock extends React.Component<{}, ClockProps> {
-  timerID: number;
-  constructor(props: ClockProps) {
+class Toggle extends React.Component<{}, { isToggleOn: boolean }> {
+  constructor(props: {}) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { isToggleOn: true };
+    this.handleClick = this.handleClick.bind(this);
   }
-  componentDidMount() {
-    this.timerID = window.setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-  tick() {
-    this.setState({
-      date: new Date()
-    });
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
   }
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
     );
   }
 }
 
-function App() {
-  return (
-    <div>
-      <Clock />
-      <Clock />
-      <Clock />
-    </div>
-  );
-}
-
 ReactDOM.render(
-  <App />,
+  <Toggle />,
   document.getElementById('root')
 );
