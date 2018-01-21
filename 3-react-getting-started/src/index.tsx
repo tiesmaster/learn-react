aa701abfc384d0492707f9381a5cca34b9cdcb60
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-01-21 22:44:08 +0100

    3: Add example with inline if.

--- 3-react-getting-started/src/index.tsx
+++ 3-react-getting-started/src/index.tsx
@@ -1,76 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-function LoginButton(props: { onClick: () => void }) {
-  return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
-  );
-}
-
-function LogoutButton(props: { onClick: () => void }) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
-}
-
-function UserGreeting(props: {}) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props: {}) {
-  return <h1>Please sign up.</h1>;
-}
-
-function Greeting(props: { isLoggedIn: boolean; children: React.ReactNode }) {
-  const isLoggedIn = props.isLoggedIn;
-  const greetingComponent = isLoggedIn
-    ? <UserGreeting />
-    : <GuestGreeting />;
+function Mailbox(props: { unreadMessages: string[] }) {
+  const unreadMessages = props.unreadMessages;
   return (
     <div>
-      {greetingComponent}
-      {props.children}
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>}
     </div>
   );
 }
 
-class LoginControl extends React.Component<{}, { isLoggedIn: boolean }> {
-  constructor(props: {}) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = { isLoggedIn: false };
-  }
-  handleLoginClick() {
-    this.setState({ isLoggedIn: true });
-  }
-  handleLogoutClick() {
-    this.setState({ isLoggedIn: false });
-  }
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-
-    let button = null;
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn}>
-          {button}
-        </Greeting>
-      </div>
-    );
-  }
-}
+const messages = ['React', 'Re: React', 'Re:Re: React'];
 
 ReactDOM.render(
-  <LoginControl />,
+  <Mailbox unreadMessages={messages} />,
   document.getElementById('root')
 );
