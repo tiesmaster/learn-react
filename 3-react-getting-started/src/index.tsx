055382c1ed6b5dd0b418e2383d0fea523fef44d2
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-01-22 23:28:30 +0100

    3: Add multiselect sample.

--- 3-react-getting-started/src/index.tsx
+++ 3-react-getting-started/src/index.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-class NameForm extends React.Component<{}, { value: string }> {
+class NameForm extends React.Component<{}, { value: string[] }> {
   constructor(props: {}) {
     super(props);
-    this.state = { value: 'coconut' };
+    this.state = { value: ['lime', 'mango'] };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event: React.FormEvent<HTMLSelectElement>) {
-    this.setState({ value: event.currentTarget.value });
+    const value = event.currentTarget.value;
+    alert(value);
+    this.setState({ value: event.currentTarget.values });
   }
   handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     alert('Your favorite flavor is: ' + this.state.value);
@@ -20,7 +22,7 @@ class NameForm extends React.Component<{}, { value: string }> {
       <form onSubmit={this.handleSubmit}>
         <label>
           Pick your favorite La Croix flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
+          <select multiple={true} value={this.state.value} onChange={this.handleChange}>
             <option value="grapefruit">Grapefruit</option>
             <option value="lime">Lime</option>
             <option value="coconut">Coconut</option>
