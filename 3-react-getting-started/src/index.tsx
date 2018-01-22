c6a5dfd7bf4a7d0323e1b800384b078b2b292d8d
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-01-22 20:50:58 +0100

    3: Convert to textarea.

--- 3-react-getting-started/src/index.tsx
+++ 3-react-getting-started/src/index.tsx
@@ -4,23 +4,25 @@ import * as ReactDOM from 'react-dom';
 class NameForm extends React.Component<{}, { value: string }> {
   constructor(props: {}) {
     super(props);
-    this.state = { value: '' };
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleChange(event: React.FormEvent<HTMLInputElement>) {
-    this.setState({ value: event.currentTarget.value.toUpperCase() });
+  handleChange(event: React.FormEvent<HTMLTextAreaElement>) {
+    this.setState({ value: event.currentTarget.value });
   }
   handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('An essay was submitted: ' + this.state.value);
     event.preventDefault();
   }
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
