c9cc51388e8ff8c00d89c2f7e9561158651cea33
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-01-28 22:44:32 +0100

    3: Add initial state.

--- 3-react-getting-started/src/index.tsx
+++ 3-react-getting-started/src/index.tsx
@@ -1,11 +1,37 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const mountNode = document.getElementById('root');
-ReactDOM.render(<input value="Ey" />, mountNode);
+function BoilingVerdict(props: { celcius: number }) {
+  if (props.celcius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
 
-window.setTimeout(
-  () => {
-    ReactDOM.render(<input value={undefined} />, mountNode);
-  },
-  3000);
+class Calculator extends React.Component<{}, { temperature: string }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { temperature: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const newTemperature = e.currentTarget.value;
+    this.setState({ temperature: newTemperature });
+  }
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celcius:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange}
+        />
+
+        <BoilingVerdict celcius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
