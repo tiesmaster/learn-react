d41178e02e0a4798c67855400fa5c95b79c1963e
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-02-11 21:15:30 +0100

    4: Also add support for TODO input.

--- 4-redux-basics/src/App.tsx
+++ 4-redux-basics/src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import AddTodo from './containers/AddTodo';
 import VisibleTodoList from './containers/VisibleTodoList';
 import Footer from './components/Footer';
 
 const App = () => (
     <div>
+        <AddTodo />
         <VisibleTodoList />
         <Footer />
     </div>
---
+++ 4-redux-basics/src/containers/AddTodo.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+
+import State from '../State';
+import { addTodo } from '../actions';
+
+const AddTodoComponent = ({ dispatch }: { dispatch: Dispatch<State> }) => {
+    let input: HTMLInputElement | null;
+
+    return (
+        <div>
+            <form
+                onSubmit={e => {
+                    e.preventDefault();
+                    if (input === null) {
+                        return;
+                    }
+                    if (input !== null && !input.value.trim()) {
+                        return;
+                    }
+                    dispatch(addTodo(input.value));
+                    input.value = '';
+                }}
+            >
+                <input
+                    ref={node => {
+                        input = node;
+                    }}
+                />
+                <button type="submit">
+                    Add Todo
+                </button>
+            </form>
+        </div>
+    );
+};
+
+const AddTodo = connect()(AddTodoComponent);
+
+export default AddTodo;
