f510d30e92c3cd07b6a90020c3b356481ebe3f56
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-02-13 01:17:27 +0100

    5: Add selection of headers.

--- 5-redux-advanced/src/components/SubredditHeader.tsx
+++ 5-redux-advanced/src/components/SubredditHeader.tsx
@@ -3,13 +3,15 @@ import * as React from 'react';
 export type SubredditHeaderProps = {
     subredditName: string;
     isSelected: boolean;
+    onClick: () => void;
 };
 
-const SubredditHeader = ({ subredditName, isSelected }: SubredditHeaderProps) => (
+const SubredditHeader = ({ subredditName, isSelected, onClick }: SubredditHeaderProps) => (
     <span
         style={{
             fontWeight: isSelected ? 'bold' : 'normal'
         }}
+        onClick={onClick}
     >
         {subredditName}
     </span>
--- 5-redux-advanced/src/containers/HeaderContainer.ts
+++ 5-redux-advanced/src/containers/HeaderContainer.ts
@@ -1,17 +1,32 @@
+import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
 import State from '../state';
-import SubredditHeader, { SubredditHeaderProps } from '../components/SubredditHeader';
+import SubredditHeader from '../components/SubredditHeader';
+import { selectSubreddit } from '../actions';
 
 type HeaderContainerProps = { subreddit: string };
 
-const mapStateToProps = (state: State, ownProps: HeaderContainerProps): SubredditHeaderProps => {
+// import { SubredditHeaderProps } from '../components/SubredditHeader';
+// TODO: figure out how to have this one return SubredditHeaderProps
+const mapStateToProps = (state: State, ownProps: HeaderContainerProps) => {
     return {
         subredditName: ownProps.subreddit,
         isSelected: state.selectedSubreddit === ownProps.subreddit
     };
 };
 
-const HeaderContainer = connect(mapStateToProps)(SubredditHeader);
+const mapDispatchToProps = (dispatch: Dispatch<State>, ownProps: HeaderContainerProps) => {
+    return {
+        onClick: () => {
+            dispatch(selectSubreddit(ownProps.subreddit));
+        }
+    };
+};
+
+const HeaderContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SubredditHeader);
 
 export default HeaderContainer;
